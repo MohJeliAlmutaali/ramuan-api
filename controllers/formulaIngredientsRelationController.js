ab94d882c14fa7ee73e9b15c562bc1be67dc3263
MohJeliAlmutaali <114728937+MohJeliAlmutaali@users.noreply.github.com>
2024-01-27 19:56:29 +0700

    Update formulaIngredientsRelationController.js

--- controllers/formulaIngredientsRelationController.js
+++ controllers/formulaIngredientsRelationController.js
@@ -88,7 +88,4 @@ const getHerbalFormulasByIngredient = (req, res) => {
     });
 };
 
-
-
-
 module.exports = { addIngredientToFormula, getIngredientsInFormula, deleteIngredientFromFormula, updateIngredientInFormula, getHerbalFormulasByIngredient };
